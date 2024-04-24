ec95971000551c7a7812dba337203b49c079eb0b
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2024-04-24 16:21:13 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.header__menubtn').click(function(){
+        $('.header.header nav').addClass('show');
+    });
+    $('.header__closemenu').click(function(){
+        $('.header.header nav').removeClass('show');
+    });
+    
+
+
+});
